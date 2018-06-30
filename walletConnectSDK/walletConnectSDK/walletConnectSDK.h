f7fb54c76170cf5bdf4b5bb60d292ac484cdfb16
Sam Ellenrieder <sam.ell@web.de>
2018-06-30 15:29:23 +0100

    project setup

---
+++ walletConnectSDK/walletConnectSDK/walletConnectSDK.h
@@ -0,0 +1,19 @@
+//
+//  walletConnectSDK.h
+//  walletConnectSDK
+//
+//  Created by Sam Ellenrieder on 30/06/2018.
+//  Copyright © 2018 WalletConnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for walletConnectSDK.
+FOUNDATION_EXPORT double walletConnectSDKVersionNumber;
+
+//! Project version string for walletConnectSDK.
+FOUNDATION_EXPORT const unsigned char walletConnectSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <walletConnectSDK/PublicHeader.h>
+
+
